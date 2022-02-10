dc34d918dbc9dd25f1f09eaf938bb99ba7172ce7
Kevin Peralta <peraltak@oregonstate.edu>
2022-02-09 20:12:52 -0800

    created rentals and rental items pages

---
+++ pages/rentals.tsx
@@ -0,0 +1,74 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Rentals: NextPage = () => {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Rentals Page</title>
+          <meta name="description" content="Rentals page" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main className={styles.main}>
+
+          <h1 className={styles.title}>Rentals</h1>
+          <p>Welcome to the Rentals page</p>
+
+          <form>
+            <fieldset>
+                <legend> Find Rental Orders Placed from the Super Duper Library Network </legend>
+                <p>Fill out zero or more of the fields below to find matching rentals</p>
+                <label>
+                    Member ID: <input type="number" name="member_ID"/>
+                </label>
+                <br/>
+                <label>
+                    Member First Name: <input type="text" name="member_first_name" />
+                </label>
+                <br/>
+                <label>
+                    Member Last Name: <input type="text" name="member_last_name" />
+                </label>
+                <br/>
+                <label>
+                    Member Phone Number: <input type="number" name="member_phone" />
+                </label>
+                <br />
+                <br />
+                <input type="submit" value="Search" />
+            </fieldset>
+            <br />
+            <fieldset>
+                <legend> Add a New Member </legend>
+                <p>Fill out the form below with the information of the new member</p>
+                <label>
+                    First Name: <input type="text" name="member_first_name" />
+                </label>
+                <br/>
+                <label>
+                    Last Name: <input type="text" name="member_last_name" />
+                </label>
+                <br/>
+                <label>
+                    Phone Number: <input type="number" name="member_phone" />
+                </label>
+                <br />
+                <br />
+                <input type="submit" value="Add Member" />
+            </fieldset>
+          </form>
+
+
+        </main>
+  
+        <footer className={styles.footer}>
+         Created by Kevin Peralta and Liam Gombart
+        </footer>
+      </div>
+    )
+  }
+  
+  export default Rentals
