ac6eb1f9d926fece68fcf318e42023a1474413f6
p-bodson <59319428+p-bodson@users.noreply.github.com>
2022-03-02 14:55:40 -0800

    front now generates query URI and sends it to backend to show a filtered search

---
+++ src/hooks/useChange.tsx
@@ -0,0 +1,17 @@
+function useChange(state: Object, setState: Function) {
+
+    function handleChange(e: any) {
+        const target = e.target;
+        const name = target.name;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+    
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
+
+    return handleChange;
+}
+
+export { useChange };
