8d3fc08782eb373c5e102e02c9294892369c7749
p-bodson <59319428+p-bodson@users.noreply.github.com>
2022-03-17 13:59:38 -0700

    double quotes cause error when unescaped, so they have been removed

--- src/components/update-form-books-and-authors.tsx
+++ src/components/update-form-books-and-authors.tsx
@@ -5,7 +5,7 @@ export default function UpdateFormBooksAndAuthors ( props: any ) {
         <fieldset>
             <legend> Edit a Book-Author Connection </legend>
             <p>Books And Authors have a simple M:M relationship.</p>
-            <p>If you want to edit the connection, just delete the one to "edit"
+            <p>If you want to edit the connection, just delete the one to edit
                 and recreate it with the updated information.</p>
             <p>Ignore the UPDATE button in the table.  It does nothing 
                 for Books {"&"} Authors.</p>
