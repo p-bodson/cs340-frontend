634a52fcb90a9949b2b7f7f7631b91562631b28a
p-bodson <59319428+p-bodson@users.noreply.github.com>
2022-03-17 16:08:45 -0700

    added noticeable text requesting browser for grading and testing

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,14 @@
 import NavBar from '@/components/navbar'
 import Footer from '@/components/footer'
+import styles from '@/styles/Home.module.css'
 
 export default function Layout({ children }: any) {
     return (
         <>
         <NavBar />
+        <p className={styles.important}>
+            Please test website with Firefox or Chrome
+        </p>
         <main>{children}</main>
         <Footer />
         </>
